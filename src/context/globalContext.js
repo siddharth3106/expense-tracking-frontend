3fbe54103a844358d23906f29f0cdfc79649d45d
Siddharth Gupta <66181834+siddharth3106@users.noreply.github.com>
2024-06-22 11:38:16 +0530

    init

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 const BASE_URL = "https://expense-tracking-backend-p016.onrender.com";
 
 
+
 const GlobalContext = React.createContext()
 
 export const GlobalProvider = ({children}) => {
